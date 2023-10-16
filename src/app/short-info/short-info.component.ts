a3a3d4251bdc1d17741af61f8d5a3c9e027f9066
VARART <vysoevn95@gmail.com>
2023-10-16 15:47:41 +0300

    On branch master     Your branch is up to date with 'dashboard/master'.     Changes to be committed: 	modified:   src/app/short-info/short-info.component.ts

--- src/app/short-info/short-info.component.ts
+++ src/app/short-info/short-info.component.ts
@@ -8,17 +8,17 @@ import { map, groupBy, mergeMap, toArray, reduce } from 'rxjs/operators';
   styleUrls: ['./short-info.component.css'],
 })
 export class ShortInfoComponent implements OnInit {
-  metrics: any[] = [];
+  metrics: Metric[] = [];
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.apiService.getData().pipe(
-      mergeMap((data) => data),
-      groupBy((item) => item.issuance_date.split('-')[1]), // Групуємо за місяцем
+      mergeMap((data) => data as any[]),
+      groupBy((item: any) => item.issuance_date.split('-')[1]),
       mergeMap((group$) =>
         group$.pipe(
-          map((item) => ({
+          map((item: any) => ({
             month: item.issuance_date.split('-')[1],
             body: +item.body,
             percent: +item.percent,
@@ -26,28 +26,33 @@ export class ShortInfoComponent implements OnInit {
           }))
         )
       ),
-      toArray(),
-      map((metrics) => {
-        return metrics.reduce((acc, metric) => {
-          const existingMetric = acc.find((m) => m.month === metric.month);
-          if (existingMetric) {
-            existingMetric.body += metric.body;
-            existingMetric.percent += metric.percent;
-            existingMetric.totalIssuances++;
-            if (metric.actual_return_date) {
-              existingMetric.totalReturns++;
-            }
-          } else {
-            metric.totalIssuances = 1;
-            metric.totalReturns = metric.actual_return_date ? 1 : 0;
-            acc.push(metric);
+      reduce((acc, metric) => {
+        const existingMetric = acc.find((m) => m.month === metric.month);
+        if (existingMetric) {
+          existingMetric.body += metric.body;
+          existingMetric.percent += metric.percent;
+          existingMetric.totalIssuances++;
+          if (metric.actual_return_date) {
+            existingMetric.totalReturns++;
           }
-          return acc;
-        }, []);
-      })
+        } else {
+          metric.totalIssuances = 1;
+          metric.totalReturns = metric.actual_return_date ? 1 : 0;
+          acc.push(metric);
+        }
+        return acc;
+      }, [] as Metric[])
     )
     .subscribe((metrics) => {
       this.metrics = metrics;
-    });
+    })
   }
+};
+
+interface Metric {
+  month: string;
+  body: number;
+  percent: number;
+  totalIssuances: number;
+  totalReturns: number;
 }
