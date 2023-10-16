98da6815f5e24a25759d0127b69e4c1c96c2163f
VARART <vysoevn95@gmail.com>
2023-10-16 15:22:18 +0300

    On branch master     Your branch is up to date with 'dashboard/master'.     Changes to be committed: 	modified:   src/app/short-info/short-info.component.html 	modified:   src/app/short-info/short-info.component.ts

--- src/app/short-info/short-info.component.ts
+++ src/app/short-info/short-info.component.ts
@@ -1,26 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service'; // Імпортуємо сервіс для роботи з даними
-import { map } from 'rxjs/operators';
+import { ApiService } from '../api.service';
+import { map, groupBy, mergeMap, toArray, reduce } from 'rxjs/operators';
 
 @Component({
   selector: 'app-short-info',
   templateUrl: './short-info.component.html',
-  styleUrls: ['./short-info.component.css']
+  styleUrls: ['./short-info.component.css'],
 })
 export class ShortInfoComponent implements OnInit {
-  data: any[] = []; // Масив для зберігання отриманих даних
-  metrics: any = {}; // Об'єкт для зберігання метрик
-  constructor(private dataService: DataService) {}
-  ngOnInit(): void { // Отримайте дані з API через сервіс
-    this.dataService.getData().pipe(
-      map((response: any) => { // Групуємо дані за місяцями та обчислюємо метрики
-        return this.calculateMetrics(response);
+  metrics: any[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getData().pipe(
+      mergeMap((data) => data),
+      groupBy((item) => item.issuance_date.split('-')[1]), // Групуємо за місяцем
+      mergeMap((group$) =>
+        group$.pipe(
+          map((item) => ({
+            month: item.issuance_date.split('-')[1],
+            body: +item.body,
+            percent: +item.percent,
+            actual_return_date: item.actual_return_date,
+          }))
+        )
+      ),
+      toArray(),
+      map((metrics) => {
+        return metrics.reduce((acc, metric) => {
+          const existingMetric = acc.find((m) => m.month === metric.month);
+          if (existingMetric) {
+            existingMetric.body += metric.body;
+            existingMetric.percent += metric.percent;
+            existingMetric.totalIssuances++;
+            if (metric.actual_return_date) {
+              existingMetric.totalReturns++;
+            }
+          } else {
+            metric.totalIssuances = 1;
+            metric.totalReturns = metric.actual_return_date ? 1 : 0;
+            acc.push(metric);
+          }
+          return acc;
+        }, []);
       })
-    ).subscribe((metrics: any) => {
-      this.metrics = metrics; // Збережіть обчислені метрики
+    )
+    .subscribe((metrics) => {
+      this.metrics = metrics;
     });
-  }
-  // Функція для обчислення метрик
-  calculateMetrics(data: any[]): any {
   }
 }
